94c8b748f1be5d2fd0f4b0d633de2fbb76d3b12b
Michael Robards <mikerobards@icloud.com>
2022-08-08 16:10:04 -0400

    build data access layer

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -44,7 +44,11 @@ function loadPlanetsData() {
     });
 }
 
+function getAllPlanets() {
+    return habitablePlanets;
+}
+
 module.exports = {
     loadPlanetsData,
-    planets: habitablePlanets,
+    getAllPlanets,
 };
